a7a97a8b6c82b51eacf6c1662a9e22af3d177a5c
friedran <friedran@gmail.com>
2013-07-03 17:56:52 +0300

    Adding menu options

--- src/com/friedran/appengine/dashboard/MainActivity.java
+++ src/com/friedran/appengine/dashboard/MainActivity.java
@@ -2,6 +2,8 @@ package com.friedran.appengine.dashboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +13,12 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
