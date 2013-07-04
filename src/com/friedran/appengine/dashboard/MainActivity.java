b61b40cc8bbe23ac5b63bd95cac543cbf180ab9a
friedran <friedran@gmail.com>
2013-07-04 11:07:16 +0300

    Added a DrawerLayout

--- src/com/friedran/appengine/dashboard/MainActivity.java
+++ src/com/friedran/appengine/dashboard/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
     /**
@@ -18,6 +20,10 @@ public class MainActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.setTitle("AppName");
         actionBar.setSubtitle("account@google.com");
+
+        String[] appsList = {"AppName1", "AppName2", "AppName2"};
+        ListView mNavDrawer = (ListView) findViewById(R.id.navigation_drawer);
+        mNavDrawer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, appsList));
     }
 
     @Override
