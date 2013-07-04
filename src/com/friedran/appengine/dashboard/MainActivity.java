d3cc014eab1cb8e4c1bf3e05bb2baff06f06ee5d
friedran <friedran@gmail.com>
2013-07-04 12:25:53 +0300

    Added drawer icons, implemented drawer logic

--- src/com/friedran/appengine/dashboard/MainActivity.java
+++ src/com/friedran/appengine/dashboard/MainActivity.java
@@ -2,13 +2,26 @@ package com.friedran.appengine.dashboard;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mNavDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private final String TITLE = "AppName";
+    private final String SUBTITLE = "someaccount@google.com";
+
     /**
      * Called when the activity is first created.
      */
@@ -18,12 +31,38 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         ActionBar actionBar = getActionBar();
-        actionBar.setTitle("AppName");
-        actionBar.setSubtitle("account@google.com");
+        actionBar.setTitle(TITLE);
+        actionBar.setSubtitle(SUBTITLE);
+        actionBar.setDisplayHomeAsUpEnabled(true);  // enable ActionBar app icon to behave as action to toggle nav drawer
+        actionBar.setHomeButtonEnabled(true);
 
-        String[] appsList = {"AppName1", "AppName2", "AppName2"};
-        ListView mNavDrawer = (ListView) findViewById(R.id.navigation_drawer);
+        String[] appsList = {"AppName1", "AppName2", "AppName3"};
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        mNavDrawer = (ListView) findViewById(R.id.left_drawer);
         mNavDrawer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, appsList));
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(TITLE);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(TITLE);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
     }
 
     @Override
@@ -31,5 +70,30 @@ public class MainActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
