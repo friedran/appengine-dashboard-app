a138b42958e58e12d16733d942f8667d68a4a858
friedran <friedran@gmail.com>
2013-07-07 07:13:39 +0300

    Extracted authentication logic to AppEngineDashboardAuthenticator

--- src/com/friedran/appengine/dashboard/gui/GoogleAuthenticationActivity.java
+++ src/com/friedran/appengine/dashboard/gui/GoogleAuthenticationActivity.java
@@ -33,7 +33,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class GoogleAuthenticationActivity extends Activity {
-    DefaultHttpClient mHttpClient = new DefaultHttpClient();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,126 +42,6 @@ public class GoogleAuthenticationActivity extends Activity {
     protected void onResume() {
         super.onResume();
         AccountManager accountManager = AccountManager.get(getApplicationContext());
-
-        // Gets the auth token asynchronously, calling the callback with its result.
-        //accountManager.getAuthToken(mDisplayedAccount, "ah", false, new GetAuthTokenCallback(), null);
     }
 
-    private class GetAuthTokenCallback implements AccountManagerCallback {
-        public void run(AccountManagerFuture result) {
-            Bundle bundle;
-            try {
-                bundle = (Bundle) result.getResult();
-                Intent intent = (Intent)bundle.get(AccountManager.KEY_INTENT);
-                if(intent != null) {
-                    // User input required
-                    startActivity(intent);
-                } else {
-                    onGetAuthToken(bundle);
-                }
-            } catch (OperationCanceledException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (AuthenticatorException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    };
-
-    protected void onGetAuthToken(Bundle bundle) {
-        String authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
-        Log.i("MainActivity", "onGetAuthToken: Got the auth token " + authToken + ", starting to load apps");
-        new LoadAppsTask().execute(authToken);
-    }
-
-    private class LoadAppsTask extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected Boolean doInBackground(String... params) {
-            try {
-                String authToken = (String) params[0];
-
-                // Don't follow redirects
-                mHttpClient.getParams().setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, false);
-
-                String url = "https://appengine.google.com/_ah/login?continue=http://localhost/&auth=" + authToken;
-                Log.i("LoadAppsTask", "Executing GET request: " + url);
-                HttpGet httpGet = new HttpGet(url);
-                HttpResponse response;
-                response = mHttpClient.execute(httpGet);
-                if(response.getStatusLine().getStatusCode() != 302)
-                    // Response should be a redirect
-                    return false;
-
-                for(Cookie cookie : mHttpClient.getCookieStore().getCookies()) {
-                    Log.i("LoadAppsTask", "Cookie name: " + cookie.getName());
-                    if(cookie.getName().equals("SACSID"))
-                        return true;
-                }
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
-                mHttpClient.getParams().setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, true);
-            }
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean result) {
-            new AuthenticatedRequestTask().execute("https://appengine.google.com/");
-        }
-    }
-
-    private class AuthenticatedRequestTask extends AsyncTask<String, Void, HttpResponse> {
-        @Override
-        protected HttpResponse doInBackground(String... params) {
-            try {
-                HttpGet http_get = new HttpGet((String) params[0]);
-                return mHttpClient.execute(http_get);
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(final HttpResponse result) {
-            new Thread() {
-                @Override
-                public void run() {
-                    Log.i("AuthenticatedRequestTask", entityToString(result.getEntity()));
-                }
-            }.start();
-        }
-    }
-
-    private static String entityToString(HttpEntity entity) {
-        StringBuilder str = new StringBuilder();
-        try {
-            InputStream is = entity.getContent();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
-
-            String line = null;
-
-            while ((line = bufferedReader.readLine()) != null) {
-                str.append(line + "\n");
-            }
-
-            is.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return str.toString();
-    }
 }
