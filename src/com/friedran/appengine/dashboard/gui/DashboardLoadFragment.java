c214621569dbf5a35da497512dfc781ad45524bb
friedran <friedran@gmail.com>
2013-07-08 12:45:16 +0300

    Changes the chart dynamically according to the selected options

--- src/com/friedran/appengine/dashboard/gui/DashboardLoadFragment.java
+++ src/com/friedran/appengine/dashboard/gui/DashboardLoadFragment.java
@@ -9,24 +9,27 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
+import android.widget.*;
 import com.friedran.appengine.dashboard.R;
 import com.friedran.appengine.dashboard.client.AppEngineDashboardClient;
 
 import java.io.InputStream;
 
-public class DashboardLoadFragment extends Fragment {
+public class DashboardLoadFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     public static final String CHART_URL_BACKGROUND_COLOR_SUFFIX = "&chf=bg,s,E8E8E8";
     private AppEngineDashboardClient mAppEngineClient;
     private String mApplicationId;
+    private int mDisplayedMetricTypeID;
+    private int mDisplayedTimeWindowID;
+
+    private Spinner mMetricSpinner;
+    private Spinner mTimeSpinner;
 
     public DashboardLoadFragment(AppEngineDashboardClient client, String applicationID) {
         mAppEngineClient = client;
         mApplicationId = applicationID;
+        resetDisplayedOptions();
     }
 
     @Override
@@ -34,44 +37,76 @@ public class DashboardLoadFragment extends Fragment {
                              Bundle savedInstanceState) {
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.dashboard_load_fragment, container, false);
 
-        setSpinnerItems(layout, R.array.load_metrics, R.id.load_chart_metric_spinner);
-        setSpinnerItems(layout, R.array.load_time_options, R.id.load_chart_time_spinner);
+        mMetricSpinner = setSpinnerWithItems(layout, R.array.load_metric_options, R.id.load_chart_metric_spinner);
+        mTimeSpinner = setSpinnerWithItems(layout, R.array.load_time_options, R.id.load_chart_time_spinner);
 
         return layout;
     }
 
-    private void setSpinnerItems(LinearLayout layout, int optionsListResourceID, int spinnerResourceID) {
+    private Spinner setSpinnerWithItems(LinearLayout layout, int optionsListResourceID, int spinnerResourceID) {
+        Spinner spinner = (Spinner) layout.findViewById(spinnerResourceID);
+
+        // Set options list
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 optionsListResourceID, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ((Spinner) layout.findViewById(spinnerResourceID)).setAdapter(adapter);
+        spinner.setAdapter(adapter);
+
+        // Set listener
+        spinner.setOnItemSelectedListener(this);
+
+        return spinner;
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        executeLoadingChart();
+        executeLoadingChartIfChanged();
+    }
+
+    /**
+     * Called when one of the spinners is changed
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        executeLoadingChartIfChanged();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Do Nothing
     }
 
     /**
      * Gets and renders the chart URL asynchronously
      */
-    private void executeLoadingChart() {
-        mAppEngineClient.executeGetChartUrl(mApplicationId, 0, 0, new AppEngineDashboardClient.PostExecuteCallback() {
-            @Override
-            public void run(Bundle result) {
-                if (!result.getBoolean(AppEngineDashboardClient.KEY_RESULT)) {
-                    // TODO: Display an error message
-                    Log.e("DashboardLoadFragment", "GetChartURL has failed");
-                    return;
-                }
+    private void executeLoadingChartIfChanged() {
+        final int selectedMetricType = mMetricSpinner.getSelectedItemPosition();
+        final int selectedTimeWindow = mTimeSpinner.getSelectedItemPosition();
 
-                String chartUrl = result.getString(AppEngineDashboardClient.KEY_CHART_URL);
-                new DownloadImageTask((ImageView) getActivity().findViewById(R.id.load_chart_image))
-                        .execute(chartUrl + CHART_URL_BACKGROUND_COLOR_SUFFIX);
-            }
-        });
+        if (selectedMetricType == mDisplayedMetricTypeID && selectedTimeWindow == mDisplayedTimeWindowID)
+            return;
+
+        mDisplayedMetricTypeID = selectedMetricType;
+        mDisplayedTimeWindowID = selectedTimeWindow;
+
+        mAppEngineClient.executeGetChartUrl(mApplicationId, selectedMetricType, selectedTimeWindow,
+                new AppEngineDashboardClient.PostExecuteCallback() {
+                    @Override
+                    public void run(Bundle result) {
+                        if (!result.getBoolean(AppEngineDashboardClient.KEY_RESULT)) {
+                            // TODO: Display an error message
+                            Log.e("DashboardLoadFragment", "GetChartURL has failed");
+                            resetDisplayedOptions();
+                            return;
+                        }
+
+                        String chartUrl = result.getString(AppEngineDashboardClient.KEY_CHART_URL);
+                        new DownloadImageTask((ImageView) getActivity().findViewById(R.id.load_chart_image))
+                                .execute(chartUrl + CHART_URL_BACKGROUND_COLOR_SUFFIX);
+                    }
+                });
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -90,6 +125,7 @@ public class DashboardLoadFragment extends Fragment {
             } catch (Exception e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
+                resetDisplayedOptions();
             }
             return mIcon11;
         }
@@ -97,5 +133,10 @@ public class DashboardLoadFragment extends Fragment {
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
         }
+    }
+
+    private void resetDisplayedOptions() {
+        mDisplayedMetricTypeID = -1;
+        mDisplayedTimeWindowID = -1;
     }
 }
