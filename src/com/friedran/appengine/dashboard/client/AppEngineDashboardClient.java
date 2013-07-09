b1273298708f17bd2c7be97b5d5489135d03e01c
friedran <friedran@gmail.com>
2013-07-09 09:17:59 +0300

    AppEngineClient: Moved network logic to background AsyncTask via a new callback, finalizes connection to fix exception, and removed all other threads

--- src/com/friedran/appengine/dashboard/client/AppEngineDashboardClient.java
+++ src/com/friedran/appengine/dashboard/client/AppEngineDashboardClient.java
@@ -21,14 +21,12 @@ import android.util.Log;
 import com.friedran.appengine.dashboard.utils.AppEngineParserUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -80,49 +78,27 @@ public class AppEngineDashboardClient {
      */
     public void executeGetApplications(final PostExecuteCallback postGetApplicationsCallback) {
         new AuthenticatedRequestTask("https://appengine.google.com/",
-            new AuthenticatedRequestTaskCallback() {
+            new AuthenticatedRequestTaskBackgroundCallback() {
                 @Override
-                public void run(final HttpEntity httpEntityResult) {
-                    onPostExecuteGetApplications(httpEntityResult, postGetApplicationsCallback);
+                public Bundle run(final HttpEntity httpResponse) {
+                    Bundle result = new Bundle();
+                    try {
+
+                        mLastRetrievedApplications = AppEngineParserUtils.getApplicationIDs(httpResponse.getContent());
+                        result.putStringArrayList(KEY_APPLICATIONS, mLastRetrievedApplications);
+                        result.putBoolean(KEY_RESULT, true);
+
+                    } catch (IOException e) {
+                        Log.e("AppEngineDashboardClient", "Failed parsing the GetApplications response", e);
+                        result.putBoolean(KEY_RESULT, false);
+                    }
+                    return result;
                 }
-        }).executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+            }, postGetApplicationsCallback).executeSerially();
     }
 
     public List<String> getLastRetrievedApplications() {
         return mLastRetrievedApplications;
-    }
-
-    /**
-     * Called when the GetApplication request is done, parses the result and returns the list of applications to the given callback.
-     *
-     * @param httpEntityResult Result of the GetApplication AuthenticatedRequest
-     * @param postGetApplicationsCallback The callback that should be called with the results of GetApplications
-     */
-    private void onPostExecuteGetApplications(final HttpEntity httpEntityResult, final PostExecuteCallback postGetApplicationsCallback) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    // Read the content and parse it. That might take some time...
-                    InputStream is = httpEntityResult.getContent();
-                    mLastRetrievedApplications = AppEngineParserUtils.getApplicationIDs(is);
-                    is.close();
-
-                    Bundle result = new Bundle();
-                    result.putBoolean(KEY_RESULT, true);
-                    result.putStringArrayList(KEY_APPLICATIONS, mLastRetrievedApplications);
-                    postGetApplicationsCallback.run(result);
-
-                } catch (IOException e) {
-                    Log.e("AppEngineDashboardClient#onPostExecuteGetApplications", "Exception caught when tried to parse result", e);
-                    e.printStackTrace();
-                    Bundle result = new Bundle();
-                    result.putBoolean(KEY_RESULT, false);
-                    postGetApplicationsCallback.run(result);
-                }
-            }
-        };
-        new Thread(runnable).start();
     }
 
     /**
@@ -135,85 +111,80 @@ public class AppEngineDashboardClient {
                 "https://appengine.google.com/dashboard/stats?app_id=s~%s&type=%d&window=%d",
                 appID, chartTypeID, chartWindowID);
 
-        new AuthenticatedRequestTask(url,
-                new AuthenticatedRequestTaskCallback() {
-                    @Override
-                    public void run(final HttpEntity httpEntityResult) {
-                        onPostExecuteGetChartURL(httpEntityResult, postGetChartUrlCallback);
+        new AuthenticatedRequestTask(url, new AuthenticatedRequestTaskBackgroundCallback() {
+                @Override
+                public Bundle run(final HttpEntity httpResponseEntity) {
+                    Bundle result = new Bundle();
+
+                    try {
+                        JSONObject jsonData = new JSONObject(EntityUtils.toString(httpResponseEntity));
+                        String chart_url = jsonData.getString("chart_url");
+
+                        result.putBoolean(KEY_RESULT, true);
+                        result.putString(KEY_CHART_URL, chart_url);
+
+                    } catch (Exception e) {
+                        Log.e("AppEngineDashboardClient#onPostExecuteGetChartURL", "Exception caught when tried to parse result", e);
+                        e.printStackTrace();
+                        result.putBoolean(KEY_RESULT, false);
                     }
-                }).executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-    }
 
-    /**
-     * Called when the GetChartURL request is done, parses the result and returns the chart url to the given callback.
-     *
-     * @param httpEntityResult Result of the GetChartURL AuthenticatedRequest
-     * @param postGetChartUrlCallback The callback that should be called with the results of GetChartURL
-     */
-    private void onPostExecuteGetChartURL(final HttpEntity httpEntityResult, final PostExecuteCallback postGetChartUrlCallback) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                Bundle result = new Bundle();
-
-                try {
-                    // Parse the result as a JSON object
-                    String content = EntityUtils.toString(httpEntityResult);
-                    JSONObject jsonData = new JSONObject(content);
-                    String chart_url = jsonData.getString("chart_url");
-
-                    result.putBoolean(KEY_RESULT, true);
-                    result.putString(KEY_CHART_URL, chart_url);
-
-                } catch (Exception e) {
-                    Log.e("AppEngineDashboardClient#onPostExecuteGetChartURL", "Exception caught when tried to parse result", e);
-                    e.printStackTrace();
-                    result.putBoolean(KEY_RESULT, false);
-
-                } finally {
-                    postGetChartUrlCallback.run(result);
+                    return result;
                 }
-            }
-        };
-        new Thread(runnable).start();
+            }, postGetChartUrlCallback).executeSerially();
     }
 
     /**
      * Inner class responsible of sending authenticated requests to Google's AppEngine servers and returning its
-     *  responses via asynchronous callbacks.
+     *  response content via asynchronous callbacks.
      */
-    private class AuthenticatedRequestTask extends AsyncTask<String, Void, HttpResponse> {
+    private class AuthenticatedRequestTask extends AsyncTask<String, Void, Bundle> {
         protected String mURL;
-        protected AuthenticatedRequestTaskCallback mCallback;
+        protected AuthenticatedRequestTaskBackgroundCallback mBackgroundCallback;
+        protected AppEngineDashboardClient.PostExecuteCallback mPostExecuteCallback;
 
-        public AuthenticatedRequestTask(String url, AuthenticatedRequestTaskCallback callback) {
+        public AuthenticatedRequestTask(String url,
+                                        AuthenticatedRequestTaskBackgroundCallback backgroundCallback,
+                                        AppEngineDashboardClient.PostExecuteCallback postExecuteCallback) {
             mURL = url;
-            mCallback = callback;
+            mBackgroundCallback = backgroundCallback;
+            mPostExecuteCallback = postExecuteCallback;
+        }
+
+        public void executeSerially() {
+            executeOnExecutor(SERIAL_EXECUTOR);
         }
 
         @Override
-        protected HttpResponse doInBackground(String... params) {
+        protected Bundle doInBackground(String... params) {
+            Bundle result = new Bundle();
+
             try {
                 Log.i("AppEngineDashboardClient", "Executing authenticated request: " + mURL);
                 HttpGet httpGet = new HttpGet(mURL);
-                return mHttpClient.execute(httpGet);
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                HttpResponse response = mHttpClient.execute(httpGet);
+
+                HttpEntity responseEntity = response.getEntity();
+                result = mBackgroundCallback.run(responseEntity);
+
+                // Finalizes the connection
+                responseEntity.consumeContent();
+
+            } catch (Exception e) {
+                Log.e("AuthenticatedRequestTask", "Exception raised while handling request for " + mURL, e);
+                result.putBoolean(KEY_RESULT, false);
             }
-            return null;
+
+            return result;
         }
 
         @Override
-        protected void onPostExecute(final HttpResponse result) {
-            mCallback.run(result.getEntity());
+        protected void onPostExecute(final Bundle result) {
+            mPostExecuteCallback.run(result);
         }
     }
 
-    private interface AuthenticatedRequestTaskCallback {
-        public void run(HttpEntity result);
+    private interface AuthenticatedRequestTaskBackgroundCallback {
+        public Bundle run(HttpEntity responseEntity);
     }
 }
