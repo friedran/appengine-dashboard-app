2110e73cdafacc7d37986f4d5fef219f6fd5a3a8
friedran <friedran@gmail.com>
2013-07-22 16:36:30 +0300

    Comment fix

--- src/com/friedran/appengine/dashboard/client/AppEngineDashboardAuthenticator.java
+++ src/com/friedran/appengine/dashboard/client/AppEngineDashboardAuthenticator.java
@@ -59,8 +59,8 @@ public class AppEngineDashboardAuthenticator {
     }
 
     public void executeAuthentication() {
-        // Gets the auth token asynchronously, calling the callback with its result (uses the deprecated API which is the only
-        // one supported from API level 5).
+        // Gets the auth token asynchronously, calling the callback with its result (uses the
+        // deprecated API which is the only one supported from API level 5).
         AccountManager.get(mApplicationContext).getAuthToken(mAccount, AUTH_TOKEN_TYPE, false, new AccountManagerCallback<Bundle>() {
             public void run(AccountManagerFuture result) {
                 Bundle bundle;
